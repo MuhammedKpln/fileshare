eb8ec5ae87bbc6324c7918beb9d796ea6d116e3a
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-10-11 00:11:24 +0200

    feat: logger service. Closes #3

---
+++ lib/core/logger/logger.dart
@@ -0,0 +1,97 @@
+import 'package:boilerplate/core/di/di.dart';
+import 'package:flutter/foundation.dart';
+import 'package:injectable/injectable.dart';
+import 'package:logger/logger.dart';
+
+@LazySingleton()
+class LogService {
+  LogService(this.logger);
+  final Logger logger;
+
+  /// Log a message at level [Level.verbose].
+  void verbose(
+    dynamic message, {
+    dynamic error,
+    bool showInProd = false,
+    StackTrace? stackTrace,
+  }) {
+    if (kDebugMode) {
+      return;
+    }
+
+    logger.v(message, error, stackTrace);
+  }
+
+  /// Log a message at level [Level.debug].
+  void debug(
+    dynamic message, {
+    dynamic error,
+    bool showInProd = false,
+    StackTrace? stackTrace,
+  }) {
+    if (kDebugMode) {
+      return;
+    }
+
+    logger.d(message, error, stackTrace);
+  }
+
+  /// Log a message at level [Level.info].
+  void info(
+    dynamic message, {
+    dynamic error,
+    bool showInProd = false,
+    StackTrace? stackTrace,
+  }) {
+    if (kDebugMode) {
+      return;
+    }
+
+    logger.i(message, error, stackTrace);
+  }
+
+  /// Log a message at level [Level.warning].
+  void warn(
+    dynamic message, {
+    dynamic error,
+    bool showInProd = false,
+    StackTrace? stackTrace,
+  }) {
+    if (kDebugMode) {
+      return;
+    }
+
+    logger.w(message, error, stackTrace);
+  }
+
+  /// Log a message at level [Level.error].
+  void error(
+    dynamic message, {
+    dynamic error,
+    bool showInProd = false,
+    StackTrace? stackTrace,
+  }) {
+    if (kDebugMode) {
+      return;
+    }
+
+    logger.e(message, error, stackTrace);
+  }
+
+  /// Log a message at level [Level.wtf].
+  void wtf(
+    dynamic message, {
+    dynamic error,
+    bool showInProd = false,
+    StackTrace? stackTrace,
+  }) {
+    if (kDebugMode) {
+      return;
+    }
+
+    logger.wtf(message, error, stackTrace);
+  }
+}
+
+/// A way to get the instance of the LogIt class.
+final LogService log = getIt<LogService>();
