e677d5180d5b6c82c4c5f419784561d0bf581c06
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-11-03 00:55:27 +0100

    update build script

--- build.sh
+++ build.sh
@@ -1,3 +1,4 @@
 flutter pub run build_runner clean
 flutter pub run build_runner build --delete-conflicting-outputs
 flutter pub run easy_localization:generate -S assets/translations/ -f json
+flutter pub run easy_localization:generate -f keys -o locale_keys.g.dart
