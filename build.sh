0b9e2fd1b18956f1792326a9ca7ecb9ed5076a11
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-12-04 12:56:08 +0100

    build: watch files at end

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 flutter pub run build_runner clean
-flutter pub run build_runner watch --delete-conflicting-outputs
 flutter pub run easy_localization:generate -S assets/translations/ -f json
 flutter pub run easy_localization:generate -f keys  -S assets/translations/ -o locale_keys.g.dart
+flutter pub run build_runner watch --delete-conflicting-outputs
