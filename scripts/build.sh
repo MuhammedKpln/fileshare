ec2d89ca972b4cc6cc4af272062c3cd2a50a418b
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-12-07 12:13:14 +0100

    build: move scripts under scripts folder

---
+++ scripts/build.sh
@@ -0,0 +1,3 @@
+flutter pub run build_runner clean
+./generate_locale.sh
+flutter pub run build_runner watch --delete-conflicting-outputs
---
+++ scripts/generate_locale.sh
@@ -0,0 +1,2 @@
+flutter pub run easy_localization:generate -S assets/translations/ -f json
+flutter pub run easy_localization:generate -f keys  -S assets/translations/ -o locale_keys.g.dart
