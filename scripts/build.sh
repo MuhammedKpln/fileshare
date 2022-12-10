f441088611fac3aede8fa88ef2a4f4bb9c49019d
Muhammed Kaplan <mmuhammedkaplan0@gmail.com>
2022-12-10 11:01:03 +0100

    build: beatufiy build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,13 @@
+#!/bin/bash
+GREEN='\033[0;32m'
+CYAN='\033[0;36m'
+NC='\033[0m'
+
+printf "${CYAN} TASK: Clean build_runner\n${NC}"
 flutter pub run build_runner clean
-./generate_locale.sh
+printf "${CYAN} TASK: Generate locale files\n${NC}"
+source $(pwd)/scripts/generate_locale.sh
+printf "${GREEN} DONE: Generating locale files\n${NC}"
+
+printf "${CYAN} TASK: Watching\n${NC}"
 flutter pub run build_runner watch --delete-conflicting-outputs
--- scripts/generate_locale.sh
+++ scripts/generate_locale.sh
@@ -1,2 +1,4 @@
+#!/bin/bash
+
 flutter pub run easy_localization:generate -S assets/translations/ -f json
 flutter pub run easy_localization:generate -f keys  -S assets/translations/ -o locale_keys.g.dart
